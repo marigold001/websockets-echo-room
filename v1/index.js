7a2f32eb00aded1aded37c84406f7fbbc600854e
nevensae <neven.jo7@gmail.com>
2020-06-22 23:19:53 +0200

    echo_room

---
+++ v1/index.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+const express = require('express')
+const app = express()
+app.listen(3000, function() {
+    console.log('Server started on port 3000');
+})
+
+app.use(express.static('public'));
+
+const server = new WebSocket.Server({ port: 3200 });
+
+server.on('connection', function(client) {
+	client.on('message', function(message) {
+		console.log('Here\'s a message from client:' + message);
+		client.send(message);
+	});
+});
